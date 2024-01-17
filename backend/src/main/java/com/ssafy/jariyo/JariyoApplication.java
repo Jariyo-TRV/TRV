4f99b828bcf31c2f7977c69c4c93acf16cb76fa7
남찬현 <chanhyun0821@gmail.com>
2024-01-17 13:54:16 +0900

    [BE] FIX : maven -> gradle
    Former-commit-id: b579718fe7585e11043a2c100c46628855c2e7e6

--- backend/src/main/java/com/ssafy/jariyo/JariyoApplication.java
+++ backend/src/main/java/com/ssafy/jariyo/JariyoApplication.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f97f1322ce453f361c0781c5a308c524d327cac084f4ea2ebfee14097d4fd367
-size 321
+oid sha256:3a9f40498c4d5efceb08506ed9f33c8ee6a36cdf1df3fcd89ec9162b97ebb9c6
+size 309
--- backend/src/main/java/com/ssafy/jariyo/controller/HelloController.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:3cebb0fe475064371b6c9cb84378fe0f7b7e75dc0b0a89e4cecd7b1564349138
-size 299
