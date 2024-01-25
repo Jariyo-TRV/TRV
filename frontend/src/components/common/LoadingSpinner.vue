16b02d91ddfb9bd653b6b098d4e2d8b9745b3ec7
강준혁 <kjh9522@gmail.com>
2024-01-25 17:28:41 +0900

    [FE]-feat-예약화면수정
    Former-commit-id: 1587720d72489e14ca3ec135b1ee0a9fa22ac1b7

--- frontend/src/components/common/LoadingSpinner.vue
+++ frontend/src/components/common/LoadingSpinner.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:47bc357d5f31574aa3b616b0818384cbfcfefaef1233f67d60de5cb046fa6289
-size 627
+oid sha256:c9d8757c7189a89e5b158a043e1336cdea8b2fff5b39408dc427ea6ff26f8690
+size 155
--- frontend/src/views/Reservation/ReservationView.vue
+++ frontend/src/views/Reservation/ReservationView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9b1f0bfcec889b0af67f17d2d984ee800d924dbcde60b796e859988bcf9c8220
-size 222
+oid sha256:c5265a637fbcf76898bf7d882db722470d9689d7abf904132bfed91316539017
+size 164
