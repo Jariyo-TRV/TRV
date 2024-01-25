9ad59b863594d6e38c2f9449ae3f7aef51a801da
이지영 <abcd23589534@gmail.com>
2024-01-25 09:20:58 +0900

    [BE] REMOVE : delete appplication-oauth.properties
    Former-commit-id: a7386e8db946a8eea6d362d696d64b6f18d3f7b3

--- frontend/src/components/common/ZariyoPass.vue
+++ frontend/src/components/common/ZariyoPass.vue
@@ -1,3 +1,25 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a5818361480fb069f0c0ebeebd8583e4a1f7e0c0a601ab1b4a66f0b04eb83b84
-size 488
+<template>
+  <RouterLink class="grid-item4" :to="{ name:'ZariyopassView' }">
+      <v-card
+      width="400"
+      height="260"
+      >
+      <v-card-text style="font-size: 30px; line-height: 190px;">
+          <v-icon
+          icon="mdi-credit-card"
+          size="80"
+          color="orange"
+          ></v-icon>
+          <span>자리패스 구매하기</span>
+          </v-card-text>
+      </v-card>
+  </RouterLink>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
