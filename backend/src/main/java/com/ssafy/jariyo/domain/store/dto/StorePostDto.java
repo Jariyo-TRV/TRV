90ddd81ff869b620d9c69aed7ec8acfaf266e0c2
남찬현 <chanhyun0821@gmail.com>
2024-01-26 10:08:31 +0900

    swagger 추가 , store 목업 추가
    Former-commit-id: 4c96353059134873dbcbc2d6213e5e4ea9060a9c

--- backend/src/main/java/com/ssafy/jariyo/domain/store/dto/StorePostDto.java
+++ backend/src/main/java/com/ssafy/jariyo/domain/store/dto/StorePostDto.java
@@ -1,21 +1,3 @@
-package com.ssafy.jariyo.domain.store.dto;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-public class StorePostDto {
-    private String name;
-    private String phone;
-    private String detail;
-    private String businessNum;
-    private Boolean isWaiting;
-    private Integer tableCount;
-    private Integer rating;
-    private Integer followCount;
-    private String operationHours;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:bdcb6587debee3ea0c0a48c560767a42c207080a66cdb03768935a8b0685818e
+size 1405
--- backend/src/main/java/com/ssafy/jariyo/domain/store/service/StoreService.java
+++ backend/src/main/java/com/ssafy/jariyo/domain/store/service/StoreService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:80bbdddeae2b0a2978148d6272b7224cb343114faac5af63009c99e77ce4b8e3
-size 1472
+oid sha256:b5efb3e0c6ed400735c22db89d3dc7d2f7ee2f3df75153ffdc3bc70535287f05
+size 1458
