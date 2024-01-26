522a8fcca525d48a93f00a46ec18007d50c01c47
강준혁 <kjh9522@gmail.com>
2024-01-26 16:06:06 +0900

    HOTFIX
    Former-commit-id: 428f99275e03aa2d3de13ce1e370e6c6a1f1f3cd

--- frontend/src/components/common/LoadingSpinner.vue
+++ frontend/src/components/common/LoadingSpinner.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:966f32a008294aee53b90c23b486de405000b4efa866092cb7648117e9b4b496
-size 429
+oid sha256:5cdd598eabe974305472a6268e0eefb3c2797b6bd1b1131d8bd5d9baf9b52ead
+size 468
