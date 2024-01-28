780dc24b10fafad74262255fa3081ca9bb9a8910
남찬현 <chanhyun0821@gmail.com>
2024-01-28 16:34:52 +0900

    [BE] STYLE : playroom 목업수정
    Former-commit-id: 5e78f93d4ede65447fd2b38a7c55c3fbf17ccbb1

--- backend/src/main/java/com/ssafy/jariyo/domain/playroom/controller/PlayroomController.java
+++ backend/src/main/java/com/ssafy/jariyo/domain/playroom/controller/PlayroomController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:20e1efb56851db696ceba81e664ef512b66d848b6f8e0ea1517f9c272b5bfe25
-size 8737
+oid sha256:92c24f3c4f09963b1c61d8c8b6a1f3cc0bf45a0076772286e7ccaf42cbdde7d7
+size 8565
