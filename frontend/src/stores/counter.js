385548bd59407487ea994c23202bba0511a5cc8c
최호연 <hy8463@naver.com>
2024-01-29 17:52:39 +0900

    [FE] FEAT: 타이머 자동 로그아웃 및 로그인 연장 추가
    Former-commit-id: 34098a0b166737d6a9a45c6e1fba24193a83ae1d

--- frontend/src/stores/counter.js
+++ frontend/src/stores/counter.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6d0176e80fcacfb2c58e8280ee180d8c3cafcbf362cfe72ab0d4bdf61ec61669
-size 2575
+oid sha256:1f5eda637661685e7a2e27bebf23c307af7d4b41e427b9ff7e53ea4c3f266613
+size 901
