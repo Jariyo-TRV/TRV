3b50670038109f2aefd513b0ec3a4a16a1bf5cec
이지영 <abcd23589534@gmail.com>
2024-01-29 17:55:00 +0900

    [BE] FEAT : 로그인 연장 수정 및 예약 상세 API 추가
    Former-commit-id: 320cbc9148ef079b0c30248a167e53541d13fd40

--- backend/src/main/java/com/ssafy/jariyo/domain/user/controller/UserController.java
+++ backend/src/main/java/com/ssafy/jariyo/domain/user/controller/UserController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:437120922fd96213c9f9ad80793880db65d5c50e7cf428bcb72bb68c48fed4bd
-size 5970
+oid sha256:132994da7898b76325d9c8092bbdf8edd9b93b470a9d8ca98b6426a6c4292cf6
+size 5050
