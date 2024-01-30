7c11052d1f95804d952fc7ea3987aabaab612135
강준혁 <kjh9522@gmail.com>
2024-01-31 08:58:27 +0900

    [FE]FIX-npm audit 수정
    Former-commit-id: aa9c354793b594ce606c6ce0b221700bd772a848

--- backend/src/main/java/com/ssafy/jariyo/domain/notification/dto/NotificationRequestPostDto.java
+++ backend/src/main/java/com/ssafy/jariyo/domain/notification/dto/NotificationRequestPostDto.java
@@ -1,4 +1,3 @@
-package com.ssafy.jariyo.domain.notification.dto;
-
-public class NotificationRequestPostDto {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:a53b8557d5f65a14c591fdb92ae77480d59beceb9e6e608368af7eed162edc52
+size 95
