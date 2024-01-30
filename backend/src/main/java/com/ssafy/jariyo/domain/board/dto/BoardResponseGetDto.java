11c7015616e9fd3edaf99e25a6d961dd1bb392a3
남찬현 <chanhyun0821@gmail.com>
2024-01-30 14:33:13 +0900

    [BE] FIX : 게시판 관련 API 반환값 변경
    Former-commit-id: 0d3fd1be6016dd7d6e8647a9f20a9c7d0f3a9a6f

--- backend/src/main/java/com/ssafy/jariyo/domain/board/dto/BoardResponseGetDto.java
+++ backend/src/main/java/com/ssafy/jariyo/domain/board/dto/BoardResponseGetDto.java
@@ -2,11 +2,15 @@ package com.ssafy.jariyo.domain.board.dto;
 import com.ssafy.jariyo.domain.board.entity.BoardDomain;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class BoardResponseGetDto {
+    private Long boardId;
     private Long userId;
     private Long storeId;
     private BoardDomain domain;
     private String title;
     private String content;
+    private LocalDateTime regDate;
 }
--- frontend/src/views/board/BoardView.vue
+++ frontend/src/views/board/BoardView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2dc7e45943f3add9d59200c1bd760d05c39cf1e2426451aa546b45c2013fc0fe
-size 5593
+oid sha256:07cbe0858be216d9a8b71ff2b85b47fe48e31f29cbc52ae6049c809e8bee742e
+size 5604
