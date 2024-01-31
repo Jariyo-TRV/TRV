a2dfd8a02f34954b132d0a5ea46a9ac16f46e497
강준혁 <kjh9522@gmail.com>
2024-01-31 14:51:02 +0900

    [FE}FIX:이미지 오류코드 수정
    Former-commit-id: 4b88b2bbfcf4cba3a8159f1459c15286489d990b

--- frontend/src/components/StreamingMenuBar/LiveChat.vue
+++ frontend/src/components/StreamingMenuBar/LiveChat.vue
@@ -1,13 +1,9 @@
 <template>
-  <div style="display: grid; justify-items: center; align-items: center;">
-    <img src="../../img/chat.PNG" alt="chatExample" style="height: 90%; width: 70%; display: flex;">
-  </div>
+    <div style="display: grid; justify-items: center; align-items: center">
+        <!-- <img src="../../img/chat.PNG" alt="chatExample" style="height: 90%; width: 70%; display: flex;"> -->
+    </div>
 </template>
 
-<script setup>
+<script setup></script>
 
-</script>
-
-<style scoped>
-
-</style>
+<style scoped></style>
