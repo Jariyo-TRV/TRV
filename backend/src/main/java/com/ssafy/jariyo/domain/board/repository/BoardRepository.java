887ff0715e9bf6dd70a971f008b3617de6eb2548
남찬현 <chanhyun0821@gmail.com>
2024-01-31 10:37:50 +0900

    [BE] FEAT : board api 페이징 기능 추가
    Former-commit-id: 867efda2a378757c1899e7c1a66481979149849e

--- backend/src/main/java/com/ssafy/jariyo/domain/board/repository/BoardRepository.java
+++ backend/src/main/java/com/ssafy/jariyo/domain/board/repository/BoardRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:19d3fd7fa649482be44042dd4528707c10b46a99e67c45cd655838e0814019da
-size 675
+oid sha256:09503c17e9c123b5f3ba6ae37b3755a2be1b68d4775b5e2cc42fc7d0aaead269
+size 720
