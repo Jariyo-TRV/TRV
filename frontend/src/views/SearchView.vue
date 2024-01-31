6239a2e028880ecfa68e21c440ceedb4da49c060
최호연 <hy8463@naver.com>
2024-01-31 09:04:51 +0900

    [FE] FEAT: Navbar Component 수정
    Former-commit-id: 33f733b6abe5defa7473d99c04dffe0d546eee95

--- frontend/src/views/SearchView.vue
+++ frontend/src/views/SearchView.vue
@@ -1,13 +1,17 @@
 <template>
-    <div>
-
+    <div>list 목록입니다.</div>
+    <div v-for="item in dataGet" :key="item.storeName">
+        <p>{{ item.storeName }}</p>
     </div>
 </template>
 
 <script setup>
+import SearchList from '@/components/SearchList.vue';
+import { useCounterStore } from '@/stores/counter';
 
+const store = useCounterStore();
+const dataGet = store.dataGet;
+console.log(dataGet);
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style scoped></style>
