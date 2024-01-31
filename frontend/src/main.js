8675dfd6fc64695af3edd90e812e3c9a968b8847
최호연 <hy8463@naver.com>
2024-01-31 09:47:10 +0900

    [FE] FIX: main.js 수정
    Former-commit-id: 8f98a9943bd418e92aff6a888660e085570bf2a0

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a85a66e57a7045be3941286b364a3027b5f150c4d11c0726e10e42f67bf4954e
-size 764
+oid sha256:9f803c8141f4a7fd5b200c122f6f3bf725110788a910d8becc621175e7d961ca
+size 694
