c203bbc34b70398292f5108cea295be6517e59aa
남찬현 <chanhyun0821@gmail.com>
2024-02-01 10:17:09 +0900

    [BE] FIX : 보드 등록시 jwt 에서 user id 를 가져오도록 설정
    Former-commit-id: 82be76d123581682994968920ae80c511b128254

--- frontend/src/views/board/BoardView.vue
+++ frontend/src/views/board/BoardView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4f32b838099804cf33b58143e13d320106f0620a4ae1fc7c4ef0f62671031a2e
-size 3851
+oid sha256:0f3eaf0a37ccdc0bf578c9707d495a94775698d44872b67d815ec726445bf5f0
+size 4206
