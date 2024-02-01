c5ba2c6afb6c089169a0ecd46c1f509bea9d19a6
남찬현 <chanhyun0821@gmail.com>
2024-02-01 17:02:02 +0900

    [BE] FIX : 상점 검색로직 변경
    Former-commit-id: 1b2bc1e59f7d71ed0de7edd6de46a25d64892e8a

--- backend/src/main/java/com/ssafy/jariyo/domain/waiting/service/WaitingService.java
+++ backend/src/main/java/com/ssafy/jariyo/domain/waiting/service/WaitingService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:80000bc072235a58645a43a3d3754d22a2ac8cc3a1854f1567ae6b4936365996
-size 4883
+oid sha256:1f59955f6355eda9bd7fa00cf1e74b189cb5422a6166601f6c1932ef76c6dfc9
+size 4884
