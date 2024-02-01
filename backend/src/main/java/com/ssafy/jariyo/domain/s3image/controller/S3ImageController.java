4a2ba237cfc1cfd1cee97fbfc52ba86a3294154e
남찬현 <chanhyun0821@gmail.com>
2024-02-02 07:31:21 +0900

    [BE] FEAT : email config 파일 추가
    Former-commit-id: 42085229632a9d5febad00eca8d5de094304ffbe

--- backend/src/main/java/com/ssafy/jariyo/domain/s3image/controller/S3ImageController.java
+++ backend/src/main/java/com/ssafy/jariyo/domain/s3image/controller/S3ImageController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9b5742df340fa3570e38ae8d507165033bb2613d633cc71971c9cf8cb3e00d8a
-size 1899
+oid sha256:577c552ed38d71b82c3415a734f9ab495f8626b3d0ce3a7a8e7a55955160423b
+size 2475
--- backend/src/main/java/com/ssafy/jariyo/global/config/EmailConfig.java
+++ backend/src/main/java/com/ssafy/jariyo/global/config/EmailConfig.java
@@ -1,2 +1,3 @@
-package com.ssafy.jariyo.global.config;public class Emailconfig {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:596a040cc270c31b12b20945ef6069dd77305c9374894cd2ee4019ce49149d76
+size 1825
