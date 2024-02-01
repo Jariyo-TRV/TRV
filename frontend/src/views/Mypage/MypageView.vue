69a5ad6b0d8da9007e56cb5b0f0e22826612755f
강준혁 <kjh9522@gmail.com>
2024-02-02 00:30:10 +0900

    [FE]feat:로그인,로그아웃페이지수정
    Former-commit-id: 758cfb23153d20c2669e2d98c0b73635426ae6d9

--- frontend/src/views/Mypage/MypageView.vue
+++ frontend/src/views/Mypage/MypageView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f0f368800265a574a33a9fa4231e18d567378bac7bb80dafe597ac47f8972334
-size 2886
+oid sha256:58e9cc04071d8784a72e19c79f9d3ed64313bc63599c666fa402bfbf517c352f
+size 2986
