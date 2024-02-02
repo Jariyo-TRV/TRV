0b9800314a7065bbfc0ce27d4f766adef788c821
최호연 <hy8463@naver.com>
2024-02-02 10:40:28 +0900

    [FE] FEAT: 타이머 연장기능 fix
    Former-commit-id: 5c694c44ce8355c2ef42e2c82b3e98a2160764d2

--- frontend/src/components/Home/Navbar.vue
+++ frontend/src/components/Home/Navbar.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:61072f018bbe1e89a6224987ea6c3292621036cf0fea6fe3c9f9a31ab6e82777
-size 8906
+oid sha256:96c6eef8c651f171360da99d163fcd449788e2366c9de426461d73ee7f041671
+size 8523
