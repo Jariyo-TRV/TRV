48e8d0bbcc3a77bf9330b8a307db7e5021a932a7
최호연 <hy8463@naver.com>
2024-02-02 10:04:32 +0900

    [FE] FEAT: storesearch페이지 수정
    Former-commit-id: c78db27fe4c952dca56b3205c5d19df4a88cd1c0

--- frontend/src/views/Store/StoreSearchView.vue
+++ frontend/src/views/Store/StoreSearchView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ea3fa03cf6f81bb7367be6cbe7a3a03f59ab9d04b2f1b0afbf0ec8ce2d80b59c
-size 6321
+oid sha256:a03f8166879632958b86cafc947c3c52e2011b4bd99d4f7d38011d120de17929
+size 7835
