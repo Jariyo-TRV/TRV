374b53e8c481f3480f1b05cb6767dbfc5b424e9d
남찬현 <chanhyun0821@gmail.com>
2024-02-02 10:01:35 +0900

    [BE] FEAT : playroom 검색기능 추가
    Former-commit-id: d34aeca1a1edb512e42475eb2c1fd2fc07620380

---
+++ backend/src/main/java/com/ssafy/jariyo/domain/playroom/dto/PlayroomResponseGetListDto.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:669e54ca090b626965b83de4fafdd26da70bff204edd4758cbfef4ee88fd3c26
+size 429
--- backend/src/main/java/com/ssafy/jariyo/domain/playroom/repository/PlayroomRepository.java
+++ backend/src/main/java/com/ssafy/jariyo/domain/playroom/repository/PlayroomRepository.java
@@ -1,14 +1,3 @@
-package com.ssafy.jariyo.domain.playroom.repository;
-
-import com.ssafy.jariyo.domain.playroom.dto.PlayroomResponseGetDto;
-import com.ssafy.jariyo.domain.playroom.entity.Playroom;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
-@Repository
-public interface PlayroomRepository extends JpaRepository<Playroom, Long> {
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:e17c73cdc34f95a2b50b813e62ac7275f5a1fd6508b2d85ba757f506dad66a9c
+size 362
