6e70c299e870a2056194d73aa6c7e1fd48b91a46
최호연 <hy8463@naver.com>
2024-02-05 13:27:53 +0900

    [FE] FEAT: streamingList Page 및 Board 기능 구현
    Former-commit-id: 77c0ccd7724708ce910c256537d56683b2e39c34

--- frontend/src/views/board/BoardDetailView.vue
+++ frontend/src/views/board/BoardDetailView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c2ba3026f695e4c01a0885bdcbd570b4b3cc5930c782faf39013da5bab0b047c
-size 2124
+oid sha256:38edeb642a61a1445446273f996b7b7c4c7e93fe0aa97e00909170fff740f964
+size 2667
