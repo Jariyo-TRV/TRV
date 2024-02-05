42b549283f974f377972262cb13fe338c91aa249
남찬현 <chanhyun0821@gmail.com>
2024-02-05 12:37:48 +0900

    [FE] FEAT : board write, dertail 페이지 추가
    Former-commit-id: a8d7622ff40f6df713c93918d042dbde7021b213

---
+++ frontend/src/views/board/BoardDetailView.vue
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c2ba3026f695e4c01a0885bdcbd570b4b3cc5930c782faf39013da5bab0b047c
+size 2124
--- frontend/src/views/board/BoardWriteView.vue
+++ frontend/src/views/board/BoardWriteView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be1fd3f106ad47102ec99e9436a6301d12528fce338f62b46bc8f896b1de6e32
-size 2087
+oid sha256:863ef6e5f39eeb7582abab777564214a3fdc4b079af6e051067327789406be79
+size 3147
