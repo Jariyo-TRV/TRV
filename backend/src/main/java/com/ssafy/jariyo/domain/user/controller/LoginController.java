f45d3f4ff8241436614b1c6c9e3f6641a2d0a53a
이지영 <abcd23589534@gmail.com>
2024-02-05 10:17:34 +0900

    [BE] FIX : login-success /api 추가
    Former-commit-id: ed73f5e13f64788322692b8809d9d69590727540

--- backend/src/main/java/com/ssafy/jariyo/domain/user/controller/LoginController.java
+++ backend/src/main/java/com/ssafy/jariyo/domain/user/controller/LoginController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b5bb29dcead44419a2d12a185caa45bf262bcd7916604e6ffc388ec838d81d4a
-size 2191
+oid sha256:6cdc2e0b6132193f50b90a2720298539c167131665ec15a7f20ab4b63fab2e8b
+size 2195
