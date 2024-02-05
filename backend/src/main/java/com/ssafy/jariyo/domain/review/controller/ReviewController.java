90aecb6db75439320db61b7cee368364e8d265ec
남찬현 <chanhyun0821@gmail.com>
2024-02-05 17:50:43 +0900

    [BE] FEAT : review 목업
    Former-commit-id: 10421ba2e469399926228348a4a7271c6fef1d93

--- backend/src/main/java/com/ssafy/jariyo/domain/review/controller/ReviewController.java
+++ backend/src/main/java/com/ssafy/jariyo/domain/review/controller/ReviewController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:940a1254fb08cdc8a9c57a96f0cd71c117f28a204c0a5b26ebcd9bb9214c0884
-size 3975
+oid sha256:2b68a3e8f252fd30a1900be978690569ec6666182264882a68a76ac10f40398a
+size 6154
--- backend/src/main/java/com/ssafy/jariyo/domain/review/dto/ReviewRequestPostDto.java
+++ backend/src/main/java/com/ssafy/jariyo/domain/review/dto/ReviewRequestPostDto.java
@@ -1,4 +1,3 @@
-package com.ssafy.jariyo.domain.review.dto;
-
-public class ReviewRequestPostDto {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:d80cc09015ea243e559d3805b3162b0bb7047716c9b2565a52d8e5e0c77b80f2
+size 258
