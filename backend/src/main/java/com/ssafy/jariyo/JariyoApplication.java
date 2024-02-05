5bd675d427a39e62cb5a65cf9534c853ac09c387
남찬현 <chanhyun0821@gmail.com>
2024-02-05 13:56:27 +0900

    [BE] FIX : board 수정 삭제 제한
    Former-commit-id: 710c73a241782c1bd0258f6b9b3ddfac1404ebc8

--- backend/src/main/java/com/ssafy/jariyo/JariyoApplication.java
+++ backend/src/main/java/com/ssafy/jariyo/JariyoApplication.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:423689ce8be28b0b15895d9a5a488f9d31b8e854d17f44421707ea61e5a5a2cf
-size 1783
+oid sha256:80bd992eb41d97fd8a5dbffb77dcdfe83215a09a1c0ba3ed512f03abdf252f18
+size 1729
