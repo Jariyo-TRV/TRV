5a88342ac692770e9b4c2f6ad41077a627dc7eea
이지영 <abcd23589534@gmail.com>
2024-02-05 15:57:40 +0900

    [BE] FIX : security config cors exclude
    Former-commit-id: 66353fdfcad1ea0f71e1e005d3ccb1e0d2de832c

--- frontend/src/views/Reservation/ReservationView.vue
+++ frontend/src/views/Reservation/ReservationView.vue
@@ -1,9 +1,3 @@
-<template>
-    <ReservationStep />
-</template>
-
-<script setup>
-import ReservationStep from '../../components/ReservationStep.vue';
-</script>
-
-<style scoped></style>
+version https://git-lfs.github.com/spec/v1
+oid sha256:87393549ea29df60ec9c83aabdb8042c65eb1cdfab2d1a589eb82d230e4a1b5e
+size 165
