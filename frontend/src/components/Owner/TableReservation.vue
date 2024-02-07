b3314a01198e406f87643f2190340823920aa0a2
강준혁 <kjh9522@gmail.com>
2024-02-07 16:59:49 +0900

    [FE]FEAT:백엔드연결해야해
    Former-commit-id: eb5605e31ad09836af9b75e5bf3d2599487891f6

--- frontend/src/components/Owner/TableReservation.vue
+++ frontend/src/components/Owner/TableReservation.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8485256a994c4bc967bb68e1295d44b30a3b76073dddc748baa74d75280defc2
-size 9711
+oid sha256:286f01529fa9efe86d0f0b7fd5f99246d43e87a104e2159e3e6e7b113de78796
+size 10415
