5ca28bfee19ee90f0d10524e0fe65f89d20c5383
남찬현 <chanhyun0821@gmail.com>
2024-02-07 10:29:19 +0900

    [FE] 로그인 리다이렉트 url 수정4
    Former-commit-id: 622dc280a42832888ae217a097a9e16164164fee

--- backend/src/main/java/com/ssafy/jariyo/domain/user/controller/LoginController.java
+++ backend/src/main/java/com/ssafy/jariyo/domain/user/controller/LoginController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:628fc713d3d5df95d1e4ec7872ec03782d5c034426a34aff72bc85ddd3149f2f
-size 2597
+oid sha256:05d114c0a671d110fe52baf3d36acc7ebd977ea992f049b845a443f583f82163
+size 2598
