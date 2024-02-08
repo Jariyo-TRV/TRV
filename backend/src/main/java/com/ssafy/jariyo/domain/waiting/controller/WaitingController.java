962193080af5fd6d21ec47cc4dc40e2bf50e7757
남찬현 <chanhyun0821@gmail.com>
2024-02-08 14:08:53 +0900

    [BE] 웨이팅 스웨거 수정
    Former-commit-id: 85ac1e9b2ee3e1e67c98cbad6166c543f4b1dca9

--- backend/src/main/java/com/ssafy/jariyo/domain/waiting/controller/WaitingController.java
+++ backend/src/main/java/com/ssafy/jariyo/domain/waiting/controller/WaitingController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:087ab2e938ff8d8598178041ef8a6ce6e79a18385e8f428e531664b1d1f1e4a8
-size 6357
+oid sha256:2f9a9c874cef7a689ffab7f8e44a938a1cac97b4c2d59a055e0f1ac916d3c49f
+size 6445
