8894109b8d0721fce602b8db41e54940341243c9
이지영 <abcd23589534@gmail.com>
2024-02-08 14:07:24 +0900

    [BE] FIX : 네이버&구글로그인 수정2
    Former-commit-id: fc98a12a0ceccfdb8ab5043ae436ef9563ec0a6d

--- backend/src/main/java/com/ssafy/jariyo/domain/user/controller/LoginController.java
+++ backend/src/main/java/com/ssafy/jariyo/domain/user/controller/LoginController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4a87579b4d215bf92e0a10333524ae7e9061d41872a2bf1b8b656c02f818a6a0
-size 2640
+oid sha256:70d2ec13a7d7d8c5efe7f34c77a3beb9bd5e798ceb24d133aa790ba68c971e95
+size 2619
