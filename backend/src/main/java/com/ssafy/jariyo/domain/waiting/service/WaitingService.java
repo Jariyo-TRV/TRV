726dfb64f6bc1462ecc8601fa7810f6bcc4df6cb
남찬현 <chanhyun0821@gmail.com>
2024-02-10 00:58:32 +0900

    [BE] FIX : 웨이팅 뒤로 미루기 수정
    Former-commit-id: a56849d2f32891c79b321cb4bc9e19b88f5977f9

--- backend/src/main/java/com/ssafy/jariyo/domain/waiting/service/WaitingService.java
+++ backend/src/main/java/com/ssafy/jariyo/domain/waiting/service/WaitingService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5af0cda6cacceb0c14e2ed14ea7802cc3db0be5279f3d1690657c62f49da33bb
-size 7303
+oid sha256:141b3928267918e87853abb172c4485724f7f516d42c095b624fa8feca0cae71
+size 8962
