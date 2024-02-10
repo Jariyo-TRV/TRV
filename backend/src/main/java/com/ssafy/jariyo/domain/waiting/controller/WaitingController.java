8cd9e26afa355d1ff9fe0ca5918273c172fdf041
남찬현 <chanhyun0821@gmail.com>
2024-02-10 23:24:31 +0900

    [BE] FIX : 웨이팅 로직 수정
    Former-commit-id: 50eb34b1935e2cf611f4d851e8e9c83459334dd8

--- backend/src/main/java/com/ssafy/jariyo/domain/waiting/controller/WaitingController.java
+++ backend/src/main/java/com/ssafy/jariyo/domain/waiting/controller/WaitingController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:59e16205e1e418b4bc7b9f9ff6b004efa013a97b8e2da9f780f97953d3164ea5
-size 8131
+oid sha256:439eaa4e5e5e1f9d31efd33909b80c0e275dfc57e9101b05be20dafa8beb4dbd
+size 6439
--- backend/src/main/java/com/ssafy/jariyo/domain/waiting/service/WaitingService.java
+++ backend/src/main/java/com/ssafy/jariyo/domain/waiting/service/WaitingService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:141b3928267918e87853abb172c4485724f7f516d42c095b624fa8feca0cae71
-size 8962
+oid sha256:d49f8714cc7a31153ffb6e47f38cde2e54c28e04024654b282ac1335fbb85764
+size 8680
