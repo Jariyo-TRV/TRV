765a3a58b75cd84904cdc250e8dffbecc77c0edc
남찬현 <chanhyun0821@gmail.com>
2024-02-11 17:28:21 +0900

    [BE] FIX : 요구사항에 따라 api 수정
    Former-commit-id: 745e3e5c93e94826163cae5af665243d8b8f3e93

--- backend/src/main/java/com/ssafy/jariyo/domain/playroom/entity/Playroom.java
+++ backend/src/main/java/com/ssafy/jariyo/domain/playroom/entity/Playroom.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:72ca715e14ea5c2b836ff19b859a809eea965d409e0739c7ec9e560b1d008bff
-size 1366
+oid sha256:604af3b1d4154ae5be0e9d69384bb57e36b3a04fca4d2983ccf1eaffeb1b410b
+size 1281
