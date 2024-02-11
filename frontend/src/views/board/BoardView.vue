52535377c80e23199fe6502958ab7e83a750db5d
임혜령 <hyegpfud@gmail.com>
2024-02-11 16:30:12 +0900

    [FE] FIX : board row data 수정
    Change-Id: Ic5a2c966830f9f914ac7c45fca3afb94c69d004f
    
    Former-commit-id: cfc3e99c80a87c6569b13a914b3c82bde46b8551

--- frontend/src/views/board/BoardView.vue
+++ frontend/src/views/board/BoardView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:83227bd557b481daacab138ba5bf88ff376a3f04e96e15780ade5396bc9f9777
-size 6230
+oid sha256:c951c746034c9e5c704c0f04845f35077258bced2a272d635033c20c6e3b215b
+size 6232
