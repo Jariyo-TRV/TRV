4df6e0380c76de90296602019cbb1ef87f85d592
임혜령 <hyegpfud@gmail.com>
2024-02-11 16:14:43 +0900

    [FE] FIX : header css
    Change-Id: Id9c309ed8e7ba686644a81a1113867de78943949
    
    Former-commit-id: 984e70461ee82bd78bff9d35ddb08768033414b8

--- frontend/src/components/Home/Navbar.vue
+++ frontend/src/components/Home/Navbar.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b1243f85d20f6a842ea1ad6cc857b5f3b82ca6f40351618e1d5d746078980ac1
-size 9117
+oid sha256:97266d3daad7d8332cd6c50886f381530dc609466e704ac2bb451dc4e2c0d42d
+size 9286
