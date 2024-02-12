4a175a9e298f471a5edf843141d7d3a0be89c7d7
김태희 <apples5060@naver.com>
2024-02-12 17:48:00 +0900

    [FE] FIX : 웨이팅 상태 확인, RestAPI 연동
    Change-Id: I35ae789e6002f8f3be816106dc707f6ea401f2ea
    
    Former-commit-id: 3b72f34f360aa670a0a6e1d8fa5863a3f5566f64

--- frontend/src/views/Store/CustomerStreamingView.vue
+++ frontend/src/views/Store/CustomerStreamingView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7b8ea3b2fab66d0b58e163a2e42839e19a932c8960d19a85e9e1b3894896f6d8
-size 22406
+oid sha256:a02823974a68b700de7a3df6f87a85317c7b517ccccbec0b6eff6af4b9f73c32
+size 22615
