67c71e733adc2005c0e974b76e926cc302871083
최호연 <hy8463@naver.com>
2024-02-12 16:07:17 +0900

    [FE]FEAT : OwnerStreaming Page 웨이팅 기능 연결
    Former-commit-id: 8558443fb0849ef851ecef5a7d9c44980eecb645

--- frontend/src/views/Reservation/CustomerReservationView.vue
+++ frontend/src/views/Reservation/CustomerReservationView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e08ea1d19f5f95e9132d4a1632cbdf25b6e8ffcc50af2a1d96be5f977e87e3ce
-size 10444
+oid sha256:187b78a0043e940e7a9af402f29a7323ea9aab0b592e6cc1cff21d2bff7749ae
+size 10508
--- frontend/src/views/Store/OwnerStreamingView.vue
+++ frontend/src/views/Store/OwnerStreamingView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:de914e6d7cb3142cc31b0e173ccc85fdfced80e10fdf6e1f1f3c043ba961b142
-size 23031
+oid sha256:f8fbd8dfced9ba08ffed3a6970c834951f613cd76e850f6ae328cca6ea43a77b
+size 23121
