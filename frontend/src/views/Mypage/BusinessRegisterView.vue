0fd6eea5e91eb2eeb5d1978b291bb91464c79d7f
최호연 <hy8463@naver.com>
2024-02-12 20:21:37 +0900

    [FE] FEAT: 기능 구현
    Former-commit-id: 58f4ddbbc9f58edb6f7d88b609ce8f84a727763c

--- frontend/src/views/Mypage/BusinessRegisterView.vue
+++ frontend/src/views/Mypage/BusinessRegisterView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:398b2352cdb4cb1cea52d1d602137ba34241bab8f3bb514d8cc7b94c77f4b1eb
-size 7469
+oid sha256:5edec9949a7c418e551e88a90e172741ac7b57f6bea971f70023a0a76206bbea
+size 7473
