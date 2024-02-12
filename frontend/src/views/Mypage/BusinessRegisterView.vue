d270dc48e16d282d78226a45461b3e29328ab336
김태희 <apples5060@naver.com>
2024-02-12 19:27:41 +0900

    [FE] FIX : SidebarLogin Role에 따른 컴포넌트 출력 수정, 사업자 등록 페이지 수정
    Change-Id: Icc3ba0a8b9baaeecec5522ac01798953236f60bf
    
    Former-commit-id: 0643103a0fe727bf5d1f6d8ddd0a400e9e88e46b

--- frontend/src/views/Mypage/BusinessRegisterView.vue
+++ frontend/src/views/Mypage/BusinessRegisterView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3e5676b897525fa1f8564a6efead3fcd18a27fc27ee98081962c3647ce7e7aa1
-size 7337
+oid sha256:398b2352cdb4cb1cea52d1d602137ba34241bab8f3bb514d8cc7b94c77f4b1eb
+size 7469
