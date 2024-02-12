96508d09c058e41727f7dbd332fedf5a8bd278f8
이지영 <abcd23589534@gmail.com>
2024-02-12 19:54:18 +0900

    [BE] FIX : 예약 가능 response 리스트 전환
    Former-commit-id: 4c0ac9929eaffd182f38b1a26e9575f2222900b2

--- backend/src/main/java/com/ssafy/jariyo/domain/reservation/repository/ReservationRepository.java
+++ backend/src/main/java/com/ssafy/jariyo/domain/reservation/repository/ReservationRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cdbc88a857970ccf788d7666614be0a27ea7e6ea91ab9fafc63bdb8e492e5815
-size 1812
+oid sha256:e2a338e526c9d8e8fe1e4f6fa899561356861513cfc457ed8d459c799efa3f86
+size 1797
