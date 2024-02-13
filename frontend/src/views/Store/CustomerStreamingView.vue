28c85893a62bda39a96bc8bc787d2f2e21deca10
김태희 <apples5060@naver.com>
2024-02-13 14:00:49 +0900

    [FE] FIX : 채팅 저장 기능 수정, UX 로그인후 사용하도록 블로킹
    Change-Id: Ia5dadbe261aa7ab2430004ba0e3f023cfbbe3237
    
    Former-commit-id: 354fd6407f4b12e445a2c8caa2c8c0b2e66634c7

--- frontend/src/views/Store/CustomerStreamingView.vue
+++ frontend/src/views/Store/CustomerStreamingView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3bb235f5c40922ab7c08910ffc697d75ef2aefaeaab71b3eef50d84f0e83a6f1
-size 23121
+oid sha256:09b4c00ac2587065ad4af1df11d0a085e7f5ad6610b738d912c2ffeccee890a9
+size 23736
