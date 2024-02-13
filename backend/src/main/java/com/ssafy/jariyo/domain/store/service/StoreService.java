98ed31e2f06114ba0cb86e690dbde5958d459306
이지영 <abcd23589534@gmail.com>
2024-02-13 12:31:14 +0900

    [BE] FIX : 팔로우 중복 구독 수정
    Former-commit-id: 9dc5874f23364867addd04cbf266bda529cbc776

--- backend/src/main/java/com/ssafy/jariyo/domain/store/service/StoreService.java
+++ backend/src/main/java/com/ssafy/jariyo/domain/store/service/StoreService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c6fdf1ba75cc6f0585a1cecfe754972c819395e6183a4b7fd98d7f5581787734
-size 22299
+oid sha256:ede512c1717dee30f2456d8593ec97b1976ccb994ebf3cdf7b7125b92f0227e6
+size 22517
