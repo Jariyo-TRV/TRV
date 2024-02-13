572d1d7fae650ad714fe8f85aa23a256604ba0d7
김태희 <apples5060@naver.com>
2024-02-13 12:51:08 +0900

    [FE] FIX : storeStatus 수정 및 웹캡, 마이크 입력 확인
    Change-Id: I7283f2ab641bbad1d82b3f5b49d256a284e1e440
    
    Former-commit-id: 624d4e7cbb4e7f3310ca5d1d9e8662b39145b1f7

--- backend/src/main/java/com/ssafy/jariyo/domain/reservation/dto/request/PayInfoDto.java
+++ backend/src/main/java/com/ssafy/jariyo/domain/reservation/dto/request/PayInfoDto.java
@@ -1,13 +1,3 @@
-package com.ssafy.jariyo.domain.reservation.dto.request;
-
-import lombok.*;
-
-@NoArgsConstructor
-@Getter
-@ToString
-@AllArgsConstructor
-@Builder
-public class PayInfoDto {
-    private Integer price;
-    private String itemName;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:0b25ee946e947a346c7a3f32fe7ff97344f01a5ef55d8b116992beaf6be25f80
+size 226
--- backend/src/main/java/com/ssafy/jariyo/domain/reservation/dto/request/PayRequest.java
+++ backend/src/main/java/com/ssafy/jariyo/domain/reservation/dto/request/PayRequest.java
@@ -1,14 +1,3 @@
-package com.ssafy.jariyo.domain.reservation.dto.request;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.ToString;
-import org.springframework.util.LinkedMultiValueMap;
-
-@Getter
-@ToString
-@AllArgsConstructor
-public class PayRequest {
-    private String url;
-    private LinkedMultiValueMap<String,String> map;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:e6337efa7a46b4e4e1c8a59ebadf4774466148c314ab9d9119de630bc054310f
+size 334
