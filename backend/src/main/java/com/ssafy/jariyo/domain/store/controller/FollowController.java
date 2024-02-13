6ff72b76a7372bfa02af3632abf5e5312cc5c90c
이지영 <abcd23589534@gmail.com>
2024-02-13 12:59:31 +0900

    [BE] FIX : 팔로우 구독&구독취소 찐찐찐막
    Former-commit-id: 47cbf312395e2004467a1b232d6a901061952789

--- backend/src/main/java/com/ssafy/jariyo/domain/store/controller/FollowController.java
+++ backend/src/main/java/com/ssafy/jariyo/domain/store/controller/FollowController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fad5f54c7c77616e6f147c3d02d8c532f3c229d9becccadc69ec8bdedf5e1176
-size 4615
+oid sha256:cd5de371a9aa3d3947834c7c8718ba89eed3455bff0d395da6d16452b71641c9
+size 4607
