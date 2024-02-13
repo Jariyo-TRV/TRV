66cbcd56c6370939a6002518d3f666f7a4be0aea
김태희 <apples5060@naver.com>
2024-02-13 14:00:49 +0900

    [FE] FIX : 채팅 저장 기능 수정, UX 로그인후 사용하도록 블로킹
    Change-Id: Ia5dadbe261aa7ab2430004ba0e3f023cfbbe3237
    
    Former-commit-id: 53282ea6fdc14d4766c4f57cb252dcc548207702

--- frontend/src/components/StreamingMenuBar/WaitingDialog.vue
+++ frontend/src/components/StreamingMenuBar/WaitingDialog.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7fff89dab56f8f60450757c8aa31e2d2e3be7998ca4cc59c8c8943a1c308694d
-size 6400
+oid sha256:d9e75565599b87e9739a3fae00fdef76ed4554235ea51686d09284554dfa10fd
+size 6532
