b53fdfddb2e0ed4f6222c0fa7401c3cee393b311
김태희 <apples5060@naver.com>
2024-02-13 19:36:13 +0900

    [FE] FIX : 구독하기 로그인 이후 사용으로 수정
    Change-Id: I2c493d2719488658b666149ceb1e2983a6addc61
    
    Former-commit-id: ec5c2ed88ae255c4327f06c871117231e5a9fcc7

--- frontend/src/views/Store/CustomerStreamingView.vue
+++ frontend/src/views/Store/CustomerStreamingView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4ef3ef86953ad8bbf5d26daaa37407637ace15aa8abf294145eac97bc1027566
-size 26080
+oid sha256:8562d3448d16c193e7a6693b30b1159db9dba44667e3081fd83333e30f31da33
+size 26131
