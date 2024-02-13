165e7cbbb91614d04a1d819e150befa35107559e
남찬현 <chanhyun0821@gmail.com>
2024-02-13 14:51:36 +0900

    [BE] FEAT : 웨이팅 만료를 정각으로 고정함
    Former-commit-id: 7bca4ca7cf68bb4e2f94f9b510665a39cd205b59

--- frontend/src/views/Store/StoreSearchView.vue
+++ frontend/src/views/Store/StoreSearchView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be15c4ee434637cc1c9a43faff99078a656c6855beee9aecc574a7ed2aae0283
-size 10856
+oid sha256:416490b4b93a7a466232835a35a1ea226f452d2c089d6e913b5df0f215eb37d8
+size 7553
