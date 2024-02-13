4c9cd83ae1582206ee5da374aab6ce1b0c5b230b
최호연 <hy8463@naver.com>
2024-02-13 11:00:31 +0900

    [FE] FEAT: 구독 기능 구현1
    Former-commit-id: 018083d578551cb66e8cfde9bb58bb9c1f9b5972

--- frontend/src/views/Mypage/BusinessRegisterView.vue
+++ frontend/src/views/Mypage/BusinessRegisterView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5edec9949a7c418e551e88a90e172741ac7b57f6bea971f70023a0a76206bbea
-size 7473
+oid sha256:e5193925f215d352164e8a079550c56b95ef01fee4a6ab133498a3d84771fb96
+size 8365
