c2a65fc69907fbef11c18198987fe354ec7e9454
김태희 <apples5060@naver.com>
2024-02-14 13:02:51 +0900

    [FE] FIX : 스트리밍 페이지 스타일 수정
    Change-Id: I78bdbb7b84fbb3cc6eb4f2a5d4baf5f068097b51
    
    Former-commit-id: e6a39e973965fb5bbb189470123bd79843856b5e

--- frontend/src/views/Store/OwnerStreamingView.vue
+++ frontend/src/views/Store/OwnerStreamingView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1868c6568f3d56416d47c1160ba36f3c53ff187a337988abf9b470dbbd64c357
-size 28256
+oid sha256:6148408c4875b8580830a8aab29abaebd31cf1c57c6ff322efaf13e34f0b6089
+size 28258
