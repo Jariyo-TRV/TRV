16b3271626625435333fb822529c0a20c5b40919
남찬현 <chanhyun0821@gmail.com>
2024-02-14 15:17:01 +0900

    [BE] FIX : 상점 등록할 때 파일 업로드 로직 수정
    Former-commit-id: a6f59657727676919ded2a1f99695e1ba0c63fc3

--- backend/src/main/java/com/ssafy/jariyo/domain/store/dto/request/StoreRequestRegistDto.java
+++ backend/src/main/java/com/ssafy/jariyo/domain/store/dto/request/StoreRequestRegistDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9c9824af28acbedc8987bdc93a7913a23d8ae22c3c03f25ec4d750576f3630fc
-size 1074
+oid sha256:4a8eb067185c57af2cbdf39712db01f40e2573fe74ea802c786e7285ade401d9
+size 960
