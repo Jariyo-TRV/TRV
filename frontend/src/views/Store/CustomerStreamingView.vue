36b3c50d9398bf94cb06fc494d13d67d37ee6b4a
김태희 <apples5060@naver.com>
2024-02-14 09:09:15 +0900

    [FE] FIX : 스트리밍 페이지 프로필 및 제목 수정
    Change-Id: Ide0eac6be2c2b488724c8862f95217ce0952af48
    
    Former-commit-id: 470c7b8a3e755e8a46c4e61da6c73c3f287fd81b

--- frontend/src/views/Store/CustomerStreamingView.vue
+++ frontend/src/views/Store/CustomerStreamingView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f66a0c9cf26e0993a1ee8566434cab6b1f224a028f85c55b14165a8eec5b8e5b
-size 26166
+oid sha256:fdca868bddd318fde9634e98c49a8d4bf62c7eaeba4fac89b38f830e04ba469b
+size 26321
