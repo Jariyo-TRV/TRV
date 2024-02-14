b5dd05cd98a29f59039483f004ccd61153ca5dd0
이지영 <abcd23589534@gmail.com>
2024-02-14 19:47:25 +0900

    [BE] FIX : 예약패스 수정정
    Former-commit-id: f9616c5680cd489c9d8328cc1028df984e9cf146

--- backend/src/main/java/com/ssafy/jariyo/domain/reservation/entity/ZPass.java
+++ backend/src/main/java/com/ssafy/jariyo/domain/reservation/entity/ZPass.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:19d709d1f74ce4aa8ef29a4fb1b0a9859687ac7e85942a5851258f9bd863c4b3
-size 1224
+oid sha256:0245f304758d2ef0eac9796e863721711b439a38f0baedfad868d02d3b2bb8a2
+size 1238
