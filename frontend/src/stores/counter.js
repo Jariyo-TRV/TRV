9b7785a65f3eba0b853b24b71fe92d7809394f79
최호연 <hy8463@naver.com>
2024-02-14 20:27:53 +0900

    [FE] FEAT: zPass 적용으로 예약일수 수정
    Former-commit-id: dda2b4703165d458eed01dd117542400747dd2e2

--- frontend/src/stores/counter.js
+++ frontend/src/stores/counter.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d5f724d4e331c31563dfe0893eec415eed403e55a9fe69708b6613bd27e46e26
-size 4313
+oid sha256:ea401911a9f3fa4c483f551574e347d5e0844208cf9f00aa41f2113580c026eb
+size 4365
