391a46e4bf8249386023e6abaf23360f33d760fd
이지영 <abcd23589534@gmail.com>
2024-02-14 14:18:20 +0900

    [FE] FIX : 메인화면 수정중
    Former-commit-id: 49c9371b9c6900b9fa94487661fe4f0ebe68fd04

--- frontend/src/components/Home/LogoutHome.vue
+++ frontend/src/components/Home/LogoutHome.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:958ab673e62db487faa820aacb691fadae947d993fa91c01fba4248955b54f8b
-size 4973
+oid sha256:bd26152fcf99ecad42dbc7bb3b4f6da56a39be8c93d30267815bccd0f48a713b
+size 2227
