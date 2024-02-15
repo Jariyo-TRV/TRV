eb01c4db517dfdefaef4cb040b02fa19ae727f73
이지영 <abcd23589534@gmail.com>
2024-02-15 09:18:39 +0900

    [BE] FIX : 에약 취소 수정
    Former-commit-id: 2f3e38564acb1131ce3e46d3f34565af7bea5038

--- backend/src/main/java/com/ssafy/jariyo/domain/reservation/service/ReservationService.java
+++ backend/src/main/java/com/ssafy/jariyo/domain/reservation/service/ReservationService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1182545b4eccf7416a0880477cbe23d357dab601c2f24fabfa995b9a5eb51bc7
-size 23099
+oid sha256:e2c490141e203ce370ca807a39afbaf92ab46d269628e79d374b0d8820968250
+size 23159
--- frontend/src/components/Owner/TableReservation.vue
+++ frontend/src/components/Owner/TableReservation.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b6df9e179161d1236e68f90172db224d9fcad9c61cea6fcad6cfcd8c820e14b1
-size 12733
+oid sha256:5bde0ffd0cd49336ce1f8e3de19a22192cb03d5876ec60fe03cf583e456ab8b3
+size 13752
