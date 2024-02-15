f7b812cc61fc099a37226ca1308170e72f8e77e8
김태희 <apples5060@naver.com>
2024-02-15 11:08:23 +0900

    [FE] FIX : 시청자수 수정
    Change-Id: I4bea6443e54c65c049d06d6768dd174fd6935487
    
    Former-commit-id: e0c8fdc1ea2b2c6ce900dc0149750852910a9780

--- frontend/src/views/Store/OwnerStreamingView.vue
+++ frontend/src/views/Store/OwnerStreamingView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f57deb654b2ad916d0fe31451b4cf2b5aae160bb23e29ff059f770520e871b34
-size 29080
+oid sha256:39e2815689300408bb5a6a3efc74fd055effa8faf7cabc4944aed2a35cf700c8
+size 29715
