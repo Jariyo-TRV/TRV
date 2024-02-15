871e91306550b17b3630127a83e4505c11a930dc
임혜령 <hyegpfud@gmail.com>
2024-02-15 14:44:14 +0900

    [FE] CSS : board domain select css
    Change-Id: Ifa117bce06c80cb70e71e5add1ee0a0c55685616
    
    Former-commit-id: 37400f090ec6e9c32d24db1adc2eaec2db77c9ce

--- frontend/src/views/board/BoardWriteView.vue
+++ frontend/src/views/board/BoardWriteView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:356e6580ecb598fb3490ae59aab716b6fec70cdfa0f532aa12b1779b2aa52286
-size 3359
+oid sha256:5d65eadc242e1db0a161c1ea1eb2d443b43ea7d5927e3fb29856e0afadafeb71
+size 3914
