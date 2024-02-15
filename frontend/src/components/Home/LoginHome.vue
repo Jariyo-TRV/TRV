fb515b66da774f30e262accf3a84bca47b413d79
강준혁 <kjh9522@gmail.com>
2024-02-15 10:57:57 +0900

    [FE]FEAT:예약CSS수정중
    Former-commit-id: f27c265e6a8dd543991c3bedc96d1aff180fcec6

--- frontend/src/components/Home/LoginHome.vue
+++ frontend/src/components/Home/LoginHome.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6171abaf43feda5b5c92c58f059b9592ef229b95113fa13d6113ab1fdd25bc41
-size 11600
+oid sha256:1b76037199136f017db7104db15ff2d31b9e285322835fec2017b87ea7d0759e
+size 11615
