1f6805685e8e055521f7d33e056a993709cb49cb
이지영 <abcd23589534@gmail.com>
2024-02-16 02:12:54 +0900

    [BE] FIX : 예약 패스 취소 로직 수정
    Former-commit-id: 395d56da25020b8a073b76919b1f1b7cab7ee2a3

--- backend/src/main/java/com/ssafy/jariyo/global/jwt/filter/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/ssafy/jariyo/global/jwt/filter/JwtAuthenticationFilter.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7f26bc2b7d96ac6ad70fe706044ee7f3bd60887f8d6911d6fabd82fdf8076177
+oid sha256:11c03871f9d8ae24349f28c3025deb5f7be6011a4f4653a6b8af41b6e3da3c2f
 size 12926
