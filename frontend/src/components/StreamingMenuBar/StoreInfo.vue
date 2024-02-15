f5af92acff8e238be3256e46c1ad48ede0ec1613
김태희 <apples5060@naver.com>
2024-02-16 01:14:58 +0900

    [FE] FIX : 기게 정보 이미지 CSS 수정
    Change-Id: I1303ecd20c5c7aaf7cb314445b743a48a518921e
    
    Former-commit-id: 5f15a88bddb775eef9c14a03619c5fcea3f84a36

--- frontend/src/components/StreamingMenuBar/StoreInfo.vue
+++ frontend/src/components/StreamingMenuBar/StoreInfo.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aaa453bebc1848d7243b3c330c7470e731b7848b8949d2088737719025ce7313
+oid sha256:162a895fe016d5de6e18b376a4bf260185e5c2c6bc66fd1f6eed728b02390cf3
 size 3950
