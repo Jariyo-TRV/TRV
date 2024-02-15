0ef352c6611b34e8038af9a18d4a4cf78260ac68
남찬현 <chanhyun0821@gmail.com>
2024-02-15 09:20:47 +0900

    [BE] FEAT : 썸네일 저장 기능 추가
    Former-commit-id: dd603c199481f6a9988f726120cc1cea327eb1f9

--- frontend/src/views/Store/OwnerStreamingView.vue
+++ frontend/src/views/Store/OwnerStreamingView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3fbad3707422b4e48668f5643e29ed925d5fb80430c1b4dbfa8b140deda2203f
-size 29433
+oid sha256:e2b765af636b041f6032d69a1c2e600ec7b13758393f84e671e8c26e5f280328
+size 31059
