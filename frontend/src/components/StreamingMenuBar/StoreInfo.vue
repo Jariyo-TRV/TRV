ab3d62dad1f66cfccd2e4b8a211872ce383a191a
김태희 <apples5060@naver.com>
2024-02-16 00:56:18 +0900

    [FE] FIX : 가게정보 버그 수정, 예약하기 기능 블로킹 추가
    Change-Id: I610cf35516a25f5958af0c8c90552b31bf068869
    
    Former-commit-id: 47335674463a86f8af8a1d8b25f8821cb39b289e

--- frontend/src/components/StreamingMenuBar/StoreInfo.vue
+++ frontend/src/components/StreamingMenuBar/StoreInfo.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:54afd75e04c3589df849e6d102f1ed912c2cb98f4c204e4dd2a5b2d2223ef87a
-size 3904
+oid sha256:348b635b41beac813dd49e4caae04d56df29f6b156105508cddcb1dee362b5b0
+size 3970
