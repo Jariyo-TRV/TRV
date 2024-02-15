b1a4406e97db5361abe9f6c424d91d086597da1f
남찬현 <chanhyun0821@gmail.com>
2024-02-15 15:30:32 +0900

    [BE] FEAT : 익명 사용자의 글작성 허용
    Former-commit-id: b37eb2bbcaf76c79b5fd5f23be9021d27bcb56a3

--- backend/src/main/java/com/ssafy/jariyo/domain/board/service/BoardService.java
+++ backend/src/main/java/com/ssafy/jariyo/domain/board/service/BoardService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0bfd51ce013a449e749ebe31762860a4c8f96418577f1b79303531a12ece0160
-size 6156
+oid sha256:edcb858a44245de6a1d5539faf84e407ea6cfb63d4feeba00ddc5bec9348b637
+size 6467
