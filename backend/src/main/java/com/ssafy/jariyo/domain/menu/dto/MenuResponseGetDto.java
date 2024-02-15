c80bd17750af71748d0014fe0bf80ff594e43cc6
최호연 <hy8463@naver.com>
2024-02-15 13:50:16 +0900

    [FE] FEAT: 스트리밍페이지에서 예약하기 기능 수정
    Former-commit-id: 75dd7e1cf3fd429d7bd36e3f7d0f2d63a274e2eb

--- backend/src/main/java/com/ssafy/jariyo/domain/menu/dto/MenuResponseGetDto.java
+++ backend/src/main/java/com/ssafy/jariyo/domain/menu/dto/MenuResponseGetDto.java
@@ -1,9 +1,3 @@
-package com.ssafy.jariyo.domain.menu.dto;
-
-import lombok.Data;
-
-@Data
-public class MenuResponseGetDto {
-    private String content;
-    private String images;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:75b01c54bbfd0c971504b71dce4f218d465aaa56986f2938a8d10815a76ce4d1
+size 161
