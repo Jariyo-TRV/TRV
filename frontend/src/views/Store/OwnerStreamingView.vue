058ad0758a0cfa8a1fea2d0ad4261e28c88755c0
남찬현 <chanhyun0821@gmail.com>
2024-02-15 11:15:28 +0900

    [BE] FIX : 플레이룸 생성, 퇴장 로직 조금 수정
    Former-commit-id: 945f0f47ba257688ba243159a6f1cccaa06ef56a

--- frontend/src/views/Store/OwnerStreamingView.vue
+++ frontend/src/views/Store/OwnerStreamingView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8fb566a5e7e7d8a048653b4de19ad25e05009d46d18465184537f39b96b04d65
-size 30891
+oid sha256:8408595f91e97b6a49afd94fdaf7435ebe4782187f3565df3a6bc5b397ece783
+size 31127
