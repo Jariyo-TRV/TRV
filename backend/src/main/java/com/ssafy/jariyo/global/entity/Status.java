b50f95fd4f9d1e9c992473f5d47917e1bcdd47ac
이지영 <abcd23589534@gmail.com>
2024-02-15 09:18:39 +0900

    [BE] FIX : 에약 취소 수정
    Former-commit-id: 1e73c04b903738612584b5d8bbbfb91d8d8ab953

--- backend/src/main/java/com/ssafy/jariyo/global/entity/Status.java
+++ backend/src/main/java/com/ssafy/jariyo/global/entity/Status.java
@@ -1,5 +1,3 @@
-package com.ssafy.jariyo.global.entity;
-
-public enum Status {
-    ACTIVE, INACTIVE, CANCLE
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:5b59985346ff21d84690e89cb18ac8d5634b10f143b764eee02b618395bc20ec
+size 94
