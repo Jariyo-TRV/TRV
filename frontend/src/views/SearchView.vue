a64269a11efedc51692ea4b89ea15a76b4bf00f1
남찬현 <chanhyun0821@gmail.com>
2024-02-15 17:28:30 +0900

    [FE] FEAT : 콩합검색 페이지 추가
    Former-commit-id: b866aaf4d073ba91c6e9c96d437350a4ae81af96

--- frontend/src/views/SearchView.vue
+++ frontend/src/views/SearchView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3a877cc31a3c13ea0bd0a9e1a48e1f96ec3edbd79e01714e36997ac7f9ffc3e4
-size 1506
+oid sha256:cbfa7e6b2fee06e914d0c30fff04cd69e0e263e0839b581a7c64e58b8e02c512
+size 3891
